a8d39dfb74dfe501b9d3ec00f1c0b1cc4e6fe9cd
justinchinfr <justin.chin@forgerock.com>
2023-04-25 10:58:56 -0400

    Bump Version

--- src/main/java/com/jumio/jumioAuthNode/JumioAuthNodePlugin.java
+++ src/main/java/com/jumio/jumioAuthNode/JumioAuthNodePlugin.java
@@ -63,7 +63,7 @@ import org.slf4j.LoggerFactory;
  */
 public class JumioAuthNodePlugin extends AbstractNodeAmPlugin {
 
-    static private String currentVersion = "1.0.19";
+    static private String currentVersion = "1.0.20";
     private final Logger logger = LoggerFactory.getLogger(JumioAuthNodePlugin.class);
 
 
